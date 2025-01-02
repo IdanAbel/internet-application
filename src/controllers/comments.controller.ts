8930f79699ebe2d34571cf3166fdc10b450d50da
royfreund <royfreund14@gmail.com>
2025-01-02 22:45:47 +0200

    Fixed comments controller

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,4 +1,18 @@
 import { Comment, CommentModel } from '../models/comment.model';
 import { BaseController } from './base.controller';
+import { Request, Response } from 'express';
 
-export const commentsController = new BaseController<Comment>(CommentModel);
+class CommentsController extends BaseController<Comment> {
+    constructor() {
+        super(CommentModel);
+    }
+
+    async create(req: Request, res: Response) {
+        const userId = req.params.userId;
+        console.log('userId', userId);
+        req.body = { ...req.body, owner: userId };
+        await super.create(req, res);
+    }
+}
+
+export const commentsController = new CommentsController();
